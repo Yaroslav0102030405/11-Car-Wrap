fc1f83b3b6832cd57608f2663098f961bfd71acd
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-09-12 20:38:01 +0300

    куку

---
+++ one.js
@@ -0,0 +1,55 @@
+// const num = 10;
+// const str = 'close';
+// const bol = true;
+// const n = null;
+// const f = 'Добро пожаловать';
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bol);
+// console.log(typeof n);
+// console.log(typeof f);
+
+// console.log(Number(num));
+
+// let elementWidth = '20px';
+// const result1 = Number.parseInt(elementWidth);
+// console.log(result1);
+
+// let elementHeight = '30.12px';
+// const result2 = Number.parseFloat(elementHeight);
+// console.log(result2);
+
+// const salary = 1300.123456;
+// const result3 = Number(salary.toFixed(4));
+// console.log(result3);
+
+// console.log(Math.random());
+
+// const message = 'В этой строке 26 символов.';
+// console.log(message.length);
+
+// const firstName = 'Yaroslav';
+// const lastName = 'Lukovetskyi';
+// const fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// const quantity = 100;
+// const orderMsg = `Вы заказали ${quantity} холодильников`;
+// console.log(orderMsg);
+
+// const brand = 'Samsung';
+// const normalizedBrand = brand.toUpperCase();
+// console.log(normalizedBrand);
+// console.log(brand.slice(3).toUpperCase());
+// console.log(brand[0]);
+
+// const a = 'мороз';
+// const b = 'аладин';
+// const string = 'В нашей МОРОЗ есть аладин';
+// console.log(string.includes(a.toUpperCase()));
+// console.log(string.includes(b));
+
+// console.log(5 && 6 && 'hello');
+// console.log(5 || 6 || true);
+// console.log(!true);
