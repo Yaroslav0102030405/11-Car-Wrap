f4f10aeb200a3439b46c3bbbc161b98caab52360
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-09-13 15:02:48 +0300

    уц

--- one.js
+++ one.js
@@ -53,3 +53,34 @@
 // console.log(5 && 6 && 'hello');
 // console.log(5 || 6 || true);
 // console.log(!true);
+
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employees = 4;
+// let totalSalary = 0;
+
+// for (let i = 1; i <= employees; i += 1) {
+//   //   console.log(i);
+//   const salary = Math.round(
+//     Math.random() * (maxSalary - minSalary) + minSalary,
+//   );
+
+//   console.log(`ЗП работника номер ${i} - ${salary}`);
+
+//   totalSalary += salary;
+// }
+
+// console.log('totalSalary:', totalSalary);
+
+// const min = 0;
+// const max = 5;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 2 !== 0) {
+//     console.log('четное i', i);
+//     continue;
+//   }
+//   total += i;
+// }
+// console.log(total);
