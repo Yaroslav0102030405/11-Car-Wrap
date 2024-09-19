eea326c0042f5f7273c909ef7f548381befe9ab4
Yaroslav0102030405 <lukovetskyi@gmail.com>
2024-09-19 17:29:39 +0300

    ава

--- one.js
+++ one.js
@@ -325,3 +325,14 @@ const friends = [
 // };
 
 // console.log(getAllNames(friends));
+
+const numbers = [...[1, 2, 3], ...[4, 5, 6], ...[7, 8, 9]];
+console.log(numbers);
+
+const lastWeekTemps = [1, 2, 3];
+const currentWeekTemps = [4, 5, 6];
+const nextWeekTemps = [7, 8, 9];
+
+const allTemps = [...lastWeekTemps, ...currentWeekTemps, ...nextWeekTemps];
+
+console.log(allTemps);
